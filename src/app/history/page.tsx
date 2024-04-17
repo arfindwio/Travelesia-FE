530efaee2c3c735e2f4a0d50a16facf777ab8b7b
Arfin Dwi Octavianto <arfindwioctavianto@gmail.com>
2024-04-17 20:31:28 +0700

    feat: add booking history to history page and add responsive home page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,26 +1,114 @@
-import Navbar from "@/components/Navbar";
+"use client";
+
+import { useState, useEffect } from "react";
+
+// Api
+import { getAllBookingsUser } from "@/api/bookings-endpoints";
 
 // Components
+import Navbar from "@/components/Navbar";
 import Topbar from "@/components/Topbar";
 import HistoryCard from "@/components/Card/HistoryCard";
 import HistoryDetailCard from "@/components/Card/HistoryDetailCard";
 
-const history = () => {
+interface BookingData {
+  id: number;
+  bookingCode: string;
+  adult: number;
+  child: number;
+  baby: number;
+  amount: number;
+  status: string;
+  methodPayment: string;
+  createdAt: string;
+  flight: FlightData;
+  passenger: PassengerData[];
+}
+
+interface FlightData {
+  id: number;
+  flightCode: string;
+  seatClass: string;
+  price: number;
+  departureTime: string;
+  arrivalTime: string;
+  duration: number;
+  airline: AirlineData;
+  departureTerminal: TerminalData;
+  arrivalTerminal: TerminalData;
+}
+
+interface AirlineData {
+  airlineName: string;
+}
+
+interface TerminalData {
+  terminalName: string;
+  airport: AirportData;
+}
+
+interface AirportData {
+  airportName: string;
+  city: string;
+}
+
+interface PassengerData {
+  id: number;
+  title: string;
+  fullName: string;
+  familyName: string;
+}
+
+const History = () => {
+  const [bookingData, setBookingData] = useState<BookingData[] | null>(null);
+  const [bookingId, setBookingId] = useState<number>(0);
+  let prevMonth: string | null = null;
+  let prevYear: string | null = null;
+
+  const filteredBooking = bookingData?.find((data) => data.id === bookingId);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const bookings = await getAllBookingsUser();
+      if (bookings) {
+        setBookingData(bookings);
+        setBookingId(bookings[0].id);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleChooseBooking = (idBooking: number) => {
+    setBookingId(idBooking);
+  };
+
   return (
     <>
       <Navbar />
       <Topbar />
-      <div className="flex w-full justify-between gap-4 px-20 pb-20 pt-4">
+      <div className="flex w-full justify-between gap-4 px-20 pb-10 pt-4">
         <div className="flex w-[55%] flex-col gap-2">
-          <h1 className="text-base font-bold">Maret 2023</h1>
-          <HistoryCard />
+          {bookingData?.map((booking) => {
+            const departureDate = new Date(booking.createdAt);
+            const month = departureDate.toLocaleString("default", { month: "long" });
+            const year = departureDate.getFullYear().toString();
+
+            const renderMonth = prevMonth !== month || prevYear !== year;
+
+            prevMonth = month;
+            prevYear = year;
+            return <HistoryCard key={booking.id} idBooking={bookingId} booking={booking} date={renderMonth ? `${month} ${year}` : ""} ChooseBookingDetail={handleChooseBooking} />;
+          })}
         </div>
-        <div className="flex w-[40%] flex-col gap-3 pt-3">
-          <HistoryDetailCard />
+        <div className="w-[40%] pt-3">
+          <div className="sticky top-[88px] flex w-full flex-col gap-3">
+            <HistoryDetailCard booking={filteredBooking} />
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default history;
+export default History;
